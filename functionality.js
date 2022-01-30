e6f3d5d178581d4d32c466a96a80b8948be02317
ingramwd <Ingramwd01@gmail.com>
2022-01-30 04:42:00 -0500

    used inquirer and made it display all three tables

--- functionality.js
+++ functionality.js
@@ -1,11 +1,28 @@
 const dbConnect = require('./db/connections');
+const cTable = require('console.table');
 
-//this will display our table role form the employee database
-const role = () => {
-    dbConnect.query(`select * from role`, (err, res) => {
-        return console.log(res)
+//this displays our table department form the employee database
+const department = () => {
+    dbConnect.query(`select * from department`, (err, res) => {
+        return console.table(res)
     });
 
 }
 
-module.exports = { role }
+//this will display our table role form the employee database
+const role = () => {
+    dbConnect.query(`select * from role`, (err, res) => {
+        return console.table(res)
+    });
+
+}
+
+//lastly this will display our table employee from the employee database
+const employee = () => {
+    dbConnect.query(`select * from employee`, (err, res) => {
+        return console.table(res)
+    });
+
+}
+
+module.exports = { department, role, employee }
--- server.js
+++ server.js
@@ -1,6 +1,49 @@
 const dbConnect = require("./db/connections"); //requiring a connection to the database
 const inquirer = require('inquirer'); //require inquirer
-const { role } = require('./functionality'); //importing my functions
+const { department, role, employee } = require('./functionality'); //importing my functions
+const cTable = require('console.table');
+
+// department();
+
+// role();
+
+// employee();
+
+const iniPrompt = () => {
+
+    return inquirer.prompt
+        ([
+            {
+                type: "list",
+                name: "view",
+                message: "What would you like to do?",
+                choices: [
+                    'view all departments',
+                    'view all roles',
+                    'view all employees',
+                    'add a department',
+                    'add a role',
+                    'add an employee',
+                    'update an employee role'
+                ]
+            },
+        ])
+        .then(function ({ view }) {
+            switch (view) {
+                case 'view all departments':
+                    department();
+                    break;
+                case 'view all roles':
+                    role();
+                    break;
+                case 'view all employees':
+                    employee();
+            }
+        })
+
+};
+
+iniPrompt();
 
 
-role();
+
