db636b11eec6a4c1a0d297a3d5a350ddb8d16cf4
ingramwd <Ingramwd01@gmail.com>
2022-01-30 19:22:13 -0500

    finalized all functions

--- server.js
+++ server.js
@@ -35,7 +35,130 @@ const departPrompt = () => {
 
         })
 
-}
+};
+
+const rolePrompt = () => {
+    return inquirer.prompt
+        ([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title you are adding?'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary of the role?'
+            },
+            {
+                type: 'input',
+                name: 'department',
+                message: 'What department is this role going to be in? (*insert department id*)'
+            }
+        ])
+        .then(input = (input) => {
+            console.log(input);
+            sql = 'INSERT INTO role (title, salary, department_id) VALUES (?,?,?)';
+            params = [input.title, input.salary, input.department];
+
+            dbConnect.query(sql, params, (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log(`added ${input.title} to role table`);
+                role();
+            })
+        })
+};
+
+const addEmployee = () => {
+    return inquirer.prompt
+        ([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the employees first name?'
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'What is the employees last name?'
+            },
+            {
+                type: 'input',
+                name: 'department',
+                message: 'What department is this employee going to be in? (*insert department id*)'
+            },
+            {
+                type: 'input',
+                name: 'role',
+                message: 'What role is this employee going have? (*insert role id*)'
+            }
+
+        ])
+        .then(input = (input) => {
+            console.log(input);
+            sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)`;
+            params = [input.firstName, input.lastName, input.department, input.role];
+
+            dbConnect.query(sql, params, (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log(`added ${input.firstName} ${input.lastName} into employee table`);
+                employee();
+            })
+        });
+};
+
+const updateEmployee = () => {
+    return inquirer.prompt
+        ([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the employees first name?'
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'What is the employees last name?'
+            },
+            {
+                type: 'input',
+                name: 'role',
+                message: 'Update employee going have? (*insert role id*)'
+            },
+            {
+                type: 'input',
+                name: 'manager',
+                message: 'Update manager id? (*insert manager id*)'
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is the employee id? (*employee id*)'
+            }
+
+        ])
+        .then(input = (input) => {
+            console.log(input);
+            sql = `UPDATE employee SET first_name = (?), last_name = (?), role_id = (?), manager_id =(?) WHERE id = (?)`;
+            params = [input.firstName, input.lastName, input.role, input.manager, input.id];
+
+            console.log(params);
+
+            dbConnect.query(sql, params, (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log(`updated employee table`);
+                employee();
+            })
+        });
+};
+
+
 
 const iniPrompt = () => {
 
@@ -69,6 +192,15 @@ const iniPrompt = () => {
                     break;
                 case 'add a department':
                     departPrompt();
+                    break;
+                case 'add a role':
+                    rolePrompt();
+                    break;
+                case 'add an employee':
+                    addEmployee()
+                    break;
+                case 'update an employee role':
+                    updateEmployee()
                     break;
             }
         })
